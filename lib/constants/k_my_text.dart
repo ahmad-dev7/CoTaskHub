1807613697d30eea9a2e54be8e69c85022cc8cb9
Ahmad Ali <aali.dev7@gmail.com>
2024-03-18 16:40:14 +0530

    second commit

--- lib/constants/k_my_text.dart
+++ lib/constants/k_my_text.dart
@@ -5,21 +5,18 @@ class KMyText extends StatelessWidget {
   final Color? color;
   final double? size;
   final FontWeight? weight;
-  final TextAlign? align;
   const KMyText(
     this.text, {
     super.key,
     this.color,
     this.size,
     this.weight,
-    this.align,
   });
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
-      textAlign: align,
       style: TextStyle(
         color: color ?? Colors.black,
         fontSize: size,
